648c33315ea82ce6da2637c7a06f3e867a8cca4c
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-09-01 02:46:29 +0300

    Add files via upload

---
+++ RegularExpression_HM/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularExpression_HM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.
+            string str1 = "Dennis";
+            string str2 = "Roni";
+            Console.WriteLine(str1.IfWordStratsWithD());// True
+            Console.WriteLine(str2.IfWordStratsWithD());// False
+
+            //2,3.
+            string str3 = "D123456_DG";
+            string str4 = "Funny";
+            string str5 = "JackSparrow";
+            string str6 = "Ron";
+            Console.WriteLine(str3.IfWordStratsWithDOrForJ());// True
+            Console.WriteLine(str4.IfWordStratsWithDOrForJ());// True
+            Console.WriteLine(str5.IfWordStratsWithDOrForJ());// True
+            Console.WriteLine(str6.IfWordStratsWithDOrForJ());// False
+
+            //4.
+            string str7 = "yyyA";
+            string str8 = "זזזזB";
+            Console.WriteLine(str7.ReplaceString()); //A
+            Console.WriteLine(str8.ReplaceString()); ;//B
+
+            //5.
+            string str9 = "Dennis";
+            string str10 = "dennis";
+            Console.WriteLine(str9.StartsWithBigChar());//True
+            Console.WriteLine(str10.StartsWithBigChar());//False
+
+            //6.
+            string str11 = "Dennisabc";
+            string str12 = "ABCdennis";
+            string str13 = "dennis";
+            Console.WriteLine(str11.HaveABCOrabc());//True
+            Console.WriteLine(str12.HaveABCOrabc());//True
+            Console.WriteLine(str13.HaveABCOrabc());//False
+
+            //7.
+            string str14 = "dennis greenberg";
+            string str15= "Cristiano Ronaldo";
+            string str16 = "L m";
+            Console.WriteLine(str14.FirstAndLastNamesValidation());//True
+            Console.WriteLine(str15.FirstAndLastNamesValidation());//True
+            Console.WriteLine(str16.FirstAndLastNamesValidation());//False
+
+            //8.
+            string str17 = "4.5";
+            string str18 = "44.5";
+            string str19 = "4";
+            Console.WriteLine(str17.IsDecimalNumber());//True
+            Console.WriteLine(str18.IsDecimalNumber());//True
+            Console.WriteLine(str19.IsDecimalNumber());//False
+
+            //9.
+            string str20 = "44.544";
+            Console.WriteLine(str20.IsDecimalNumberHasThreeNumsAfterPoint());//True
+            Console.WriteLine(str18.IsDecimalNumberHasThreeNumsAfterPoint());//False
+            Console.WriteLine(str19.IsDecimalNumberHasThreeNumsAfterPoint());//False
+
+            //10.
+            string str21 = "denis4167@gmail.com";
+            string str22 = "dennis@yahoo.co.il";
+            string str23 = "dennis443933@Gmailco.il";
+            Console.WriteLine(str21.EmailValidation());//True
+            Console.WriteLine(str22.EmailValidation());//True
+            Console.WriteLine(str23.EmailValidation());//False
+            
+            //11.
+            string str24 = "geeksforgeeks.co.il";
+            string str25 = "geeksforgeeks.com";
+            string str26 = "geeksforgeeks.org";
+            Console.WriteLine(str24.DomainValidation());//True
+            Console.WriteLine(str25.DomainValidation());//False
+            Console.WriteLine(str26.DomainValidation());//False
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegularExpression_HM/ValidationClasss.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression_HM
+{
+    public static class ValidationClasss
+    {
+        //1.
+        public static bool IfWordStratsWithD(this string str)
+        {
+            string pattern = @"^D";
+            return Regex.Match(str, pattern).Success;
+        }
+
+        public static bool IfWordStratsWithDOrForJ(this string str)
+        {
+            string pattern = @"^[DFJ]\d{6}\w[A-Z]{2}";
+            return Regex.Match(str, pattern).Success;
+        }
+        public static string ReplaceString(this string str)
+        {
+            string pattern = @"(y{3}|ז{4})";
+            string CleanedString = Regex.Replace(str, pattern, "");
+            return CleanedString;
+        }
+        public static bool StartsWithBigChar(this string str)
+        {
+            string pattern = @"^[A-Z]";
+            return Regex.Match(str, pattern).Success;
+        }
+        public static bool HaveABCOrabc(this string str)
+        {
+            string pattern = "(abc|ABC)";
+            return Regex.Match(str, pattern).Success;
+        }
+        public static bool FirstAndLastNamesValidation(this string str)
+        {
+            string pattern = "[A-Za-z]{2,18}[\\s][a-zA-Z]{2,18}";
+            return Regex.Match(str, pattern).Success;
+        }
+        public static bool IsDecimalNumber(this string str)
+        {
+            string pattern = @"^[0-9]{1,}\.{1}[0-9]{1,}$";
+            return Regex.Match(str, pattern).Success;
+        }
+        public static bool IsDecimalNumberHasThreeNumsAfterPoint(this string str)
+        {
+            string pattern = @"^[0-9]{1,}\.{1}[0-9]{3}$";
+            return Regex.Match(str, pattern).Success;
+        }
+        public static bool EmailValidation(this string str)
+        {
+            string pattern = @"^[a-zA-Z0-9]{2,15}\@(gmail|GMAIL|yahoo|YAHOO)\.\D{3,}$";
+            return Regex.Match(str, pattern).Success;
+        }
+        public static bool DomainValidation(this string str)
+        {
+            string pattern = "^(?!-)[A-Za-z0-9-]+([\\-\\.]{1}[a-z0-9]+)*\\.co.il$";
+            return Regex.Match(str, pattern).Success;
+        }
+
+
+    }
+}
